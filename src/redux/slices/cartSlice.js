fe663ed7b677e8b515d1be5dbc57b1ccd2f98b7d
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-01-31 07:12:53 +0200

    fix cart count

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -25,17 +25,26 @@ export const cartSlice = createSlice({
         return obj.price * obj.count + sum;
       }, 0);
     },
+
     minusItem(state, action) {
       const findItem = state.items.find((obj) => obj.id === action.payload);
-
-      if (findItem) {
+      
+      if (findItem ) {
         findItem.count--;
       }
+      if (findItem.count === 0) {
+        state.items = state.items.filter((obj) => obj.id !== action.payload);
+      }
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count - sum;
+      }, 0);
     },
+
     removeItem(state, action) {
       state.items = state.items.filter((obj) => obj.id !== action.payload);
       
     },
+
     clearItem(state) {
       state.items = [];
       state.totalPrice = 0;
