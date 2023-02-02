d1e1e0fc9bf2ba8952c647662653a5072f24170d
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-02-02 19:41:35 +0200

    fix scss

--- src/components/PizzaBlock/index.tsx
+++ src/components/PizzaBlock/index.tsx
@@ -1,3 +1,5 @@
+import { ReactComponent as PlusSvg } from '../../assets/img/plus.svg';
+
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -17,7 +19,7 @@ type PizzaBlockProps = {
 };
 const PizzaBlock: React.FC<PizzaBlockProps> = ({ id, title, price, imageUrl, sizes, types }) => {
   const dispatch = useDispatch();
-  const cartItem = useSelector((state: RootState) => state.cart.items.find((obj) => obj.id === id)); 
+  const cartItem = useSelector((state: RootState) => state.cart.items.find((obj) => obj.id === id));
 
   const [activeType, setActiveType] = useState(0);
   const [activeSize, setActiveSize] = useState(0);
@@ -32,7 +34,7 @@ const PizzaBlock: React.FC<PizzaBlockProps> = ({ id, title, price, imageUrl, siz
       imageUrl,
       type: typeNames[activeType],
       size: sizes[activeSize],
-      count: 0
+      count: 0,
     };
     dispatch(addItem(item));
   };
@@ -70,18 +72,7 @@ const PizzaBlock: React.FC<PizzaBlockProps> = ({ id, title, price, imageUrl, siz
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">від {price} ₴</div>
         <button onClick={onClickAdd} className="button button--outline button--add">
-          <svg
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
-              fill="white"
-            ></path>
-          </svg>
+          <PlusSvg />
           <span>Додати</span>
           {addedCount > 0 && <i>{addedCount}</i>}
         </button>
